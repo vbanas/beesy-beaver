94bcda590ebd0fce40130993d73abdfa26482f59
Igor <igor.myth@gmail.com>
2015-08-07 20:40:41 +0300

    Mov eoperation and tests

--- src/field.lisp
+++ src/field.lisp
@@ -45,8 +45,30 @@
 
 ;; And now for hexagonal operations
 
+(defun move (pos dir)
+  (with-slots (row col) pos
+    (let ((new-row row)
+          (new-col col))
+      (case dir
+        (:west (decf new-col))
+        (:east (incf new-col))
+        (:south-west (incf new-row)
+                     (when (evenp row)
+                       (decf new-col)))
+        (:south-east (incf new-row)
+                     (when (oddp row)
+                       (incf new-col)))
+        (otherwise (return-from move :invalid)))
+      (when (or (< new-row 0)
+                (>= new-row *height*))
+        (return-from move :invalid))
+      (when (or (< new-col 0)
+                (>= new-col *width*))
+        (return-from move :invalid))
+      (make-pos new-row new-col))))
 
 
+ 
 ;; Little benchmarking
 
 (defun benchmark-field (width height)
@@ -70,3 +92,60 @@
              do (setf field (remove-row field row))))
     (time (loop for pos in reads
                do (get-cell field pos)))))
+
+(defun test-move ()
+  (let* ((*width* 10)
+         (*height* 20)
+         (failed 0))
+    (labels ((%test (row col dir ref-res)
+               (let ((res (move (make-pos row col) dir)))
+                 (unless (equalp res ref-res)
+                   (format t "Wrong move ~A: from ~A, res = ~A, reference = ~A~%"
+                           dir
+                           (make-pos row col)
+                           res
+                           ref-res)
+                   (incf failed)))))
+
+      (%test 0 2 :east (make-pos 0 3))
+      (%test 0 8 :east (make-pos 0 9))
+      (%test 5 8 :east (make-pos 5 9))
+      (%test 19 8 :east (make-pos 19 9))
+      (%test 5 9 :east :invalid)
+      (%test 3 9 :east :invalid)
+      (%test 19 9 :east :invalid)
+
+      (%test 0 1 :west (make-pos 0 0))
+      (%test 0 9 :west (make-pos 0 8))
+      (%test 5 1 :west (make-pos 5 0))
+      (%test 19 1 :west (make-pos 19 0))
+      (%test 5 0 :west :invalid)
+      (%test 3 0 :west :invalid)
+      (%test 19 0 :west :invalid)
+
+      (%test 0 2 :south-east (make-pos 1 2))
+      (%test 0 2 :south-west (make-pos 1 1))
+
+      (%test 0 0 :south-east (make-pos 1 0))
+      (%test 0 0 :south-west :invalid)
+
+      (%test 0 9 :south-east (make-pos 1 9))
+      (%test 0 9 :south-west (make-pos 1 8))
+
+      (%test 1 2 :south-east (make-pos 2 3))
+      (%test 1 2 :south-west (make-pos 2 2))
+
+      (%test 1 0 :south-east (make-pos 2 1))
+      (%test 1 0 :south-west (make-pos 2 0))
+
+      (%test 1 9 :south-east :invalid)
+      (%test 1 9 :south-west (make-pos 2 9))
+
+      (%test 19 0 :south-east :invalid)
+      (%test 19 0 :south-west :invalid)
+      (%test 19 5 :south-east :invalid)
+      (%test 19 5 :south-west :invalid)
+      (%test 19 9 :south-east :invalid)
+      (%test 19 9 :south-west :invalid)
+
+      (format t "Tests failed: ~A~%" failed))))
