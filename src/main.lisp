fac3df6a3291c2415a4b5d99d8faa3d73701f155
Banas <banas@ubuntu.ubuntu-domain>
2015-08-08 16:39:25 +0300

    Main function fix for getting result.

--- src/main.lisp
+++ src/main.lisp
@@ -2,9 +2,10 @@
 
 (defun main ()
   (when sb-ext:*posix-argv*
-    (let* ((parsed-args (apply-argv:parse-argv* sb-ext:*posix-argv*))
+    (let* ((parsed-args (apply-argv:parse-argv* ;;'("./test" "-f" "problems/problem_0.json" "-f" "problems/problem_1.json")))
+			 sb-ext:*posix-argv*))
 	   (files) (phrases) (time) (memory) (proc-count))
-      (format t "~A~%~A~%" parsed-args (alexandria:plist-alist (cdr parsed-args)))
+      ;;(format t "~A~%~A~%" parsed-args (alexandria:plist-alist (cdr parsed-args)))
       (mapcar (lambda (p) 
 		(let ((o (string (car p)))
 		      (v (cdr p)))
@@ -15,11 +16,12 @@
 		    ((string= "-m" o) (setq memory v))
 		    ((string= "-t" o) (setq time v)))))
 	      (alexandria:plist-alist (cdr parsed-args)))
-      (format t "~A~%" files)
+      ;;(format t "~A~%" files)
       (let ((result-list nil))
-	(dolist (f files)			
+	(dolist (f (reverse files))			
 	  (when (probe-file f)
-	    (format t "~A~%~%" (alexandria:read-file-into-string f))
-	    (append result-list (simple-wave-from-task 
-				 (decode-task (alexandria:read-file-into-string f)))))) 
+	    ;;(format t "~A~%~%" (alexandria:read-file-into-string f))
+	    (setf result-list 
+		  (append result-list (simple-wave-from-task 
+				       (decode-task (alexandria:read-file-into-string f))))))) 
 	(yason:encode result-list)))))
